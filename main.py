e87502d2cf666ba6258d3b29570468688e0b828b
mehakchohan <mehakgul@umich.edu>
2025-05-29 06:26:14 -0400

    initial

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import yfinance as yf
 import matplotlib.pyplot as plt
 
-
+#Using apple as example
 symbol = "AAPL" 
 start_date = "2023-01-01"
 end_date = "2024-01-01"
@@ -18,27 +18,23 @@ plt.plot(df['Close'])
 plt.title(f"{symbol} Closing Price")
 plt.show()
 
-
 df.dropna(inplace=True)
-
 
 # Only fit on the 'close' column
 scaler = MinMaxScaler(feature_range=(0, 1))
 scaled_data = scaler.fit_transform(df[['Close']])  
 
 
-
-def create_sequences(data, seq_length=60):
+def create_sequences(data, seq_length=10):
     X, y = [], []
     for i in range(seq_length, len(data)):
-        X.append(data[i-seq_length:i])  # past 60 days as input sequence 
+        X.append(data[i-seq_length:i])  # past 10 days as input sequence 
         y.append(data[i])  # Next day's price output
     return np.array(X), np.array(y)
 
 X, y = create_sequences(scaled_data)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
-
 
 model = Sequential()
 model.add(LSTM(50, input_shape=(X_train.shape[1], X_train.shape[2])))
@@ -51,7 +47,6 @@ history = model.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=
 predicted = model.predict(X_test)
 predicted_rescaled = scaler.inverse_transform(predicted.reshape(-1, 1)).flatten()
 real_rescaled = scaler.inverse_transform(y_test.reshape(-1, 1)).flatten()
-
 
 plt.figure(figsize=(10, 5))
 plt.plot(real_rescaled, label='Actual Price')
